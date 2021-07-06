10a8f0a0e2e51e0e807f17489447262d36e4a484
Tanney102 <57767891+Tanney-102@users.noreply.github.com>
2021-07-06 21:40:16 +0900

    프론트엔드 기본 환경 설정 (#3)
    * chore: 기본 환경설정 (#2)
    
    * chore: 필수 의존성 설치 (#2)

---
+++ frontend/babel.config.ts
@@ -0,0 +1,5 @@
+const config = {
+  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+};
+
+export default config;
---
+++ frontend/webpack.config.ts
@@ -0,0 +1,49 @@
+import path from "path";
+import webpack from "webpack";
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "hidden-source-map" : "eval",
+
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve("./dist"),
+    filename: "bundle.js",
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./public/index.html", // 아까는 ./src/index.html 이었음
+    }),
+  ],
+};
+
+export default config;
