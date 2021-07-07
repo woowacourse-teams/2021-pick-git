1da6754cb1c966288433f2804b8911e0310b2fcb
Tanney <dhxodms89@gmail.com>
2021-07-07 18:08:15 +0900

    chore: 스토리북 기본설정
    Co-authored-by: SONG WON LEE <swon3210@users.noreply.github.com>

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+import { theme, GlobalStyle } from '../src/App.style';
+import { ThemeProvider } from 'styled-components';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +11,25 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: {
+      mobile: {
+        name: "mobile",
+        styles: {
+          width: "375px",
+          height: "812px",
+        },
+      }
+    }
+  }
 }
+
+addDecorator(Story => (
+    <MemoryRouter initialEntries={['/']}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story />
+      </ThemeProvider>
+    </MemoryRouter>
+  )
+)
