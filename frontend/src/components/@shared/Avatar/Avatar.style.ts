7d74636013d0f0cbf8c45504f136f2eed1fd18b3
Tanney <dhxodms89@gmail.com>
2021-07-08 00:58:18 +0900

    feat: Avatar 컴포넌트 구현 (#13)

---
+++ frontend/src/components/@shared/Avatar/Avatar.style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+import defaultProfile from "../../../assets/images/defaultProfile.svg";
+
+export const Container = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CircleImage = styled.div<React.CSSProperties>`
+  ${({ width, height, backgroundImage }) => `
+    width: ${width};
+    height: ${height};
+    background: no-repeat center/cover url(${backgroundImage ?? defaultProfile});
+  `}
+
+  border-radius: 50%;
+`;
+
+export const Name = styled.div<React.CSSProperties>`
+  ${({ fontSize, theme }) => `
+    font-size: ${fontSize ?? "1rem"};
+    color: ${theme.textColor};
+  `}
+
+  margin-top: 0.375rem;
+`;
