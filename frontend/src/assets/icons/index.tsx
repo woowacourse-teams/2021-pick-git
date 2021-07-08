c3492ad0df7232ab77c7ca1558f866ef215c034f
LEESONGWON <swon3210@skku.edu>
2021-07-08 15:06:36 +0900

    chore: svg 파일 추가 및 svg 컴포넌트를 하나의 파일에서 import 해올 수 있도록 설정

---
+++ frontend/src/assets/icons/index.tsx
@@ -0,0 +1,5 @@
+export { ReactComponent as HomeIcon } from "./home.svg";
+export { ReactComponent as PersonIcon } from "./person.svg";
+export { ReactComponent as AddBoxIcon } from "./add-box.svg";
+export { ReactComponent as SearchIcon } from "./search.svg";
+export { ReactComponent as LoginIcon } from "./login.svg";
