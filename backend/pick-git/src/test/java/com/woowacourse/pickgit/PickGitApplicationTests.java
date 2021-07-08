92de63d07000d90db1a16b2c1a5fa820701a4582
DAEUN LEE <leede0418@gmail.com>
2021-07-08 15:33:10 +0900

    chore: 백엔드 기본 환경 설정 (#30)

---
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/PickGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.pickgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
