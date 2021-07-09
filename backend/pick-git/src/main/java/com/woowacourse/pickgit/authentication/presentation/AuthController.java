08c271dede92bcdde4d1ddb99068dd4f1865f184
binghe819 <binghe819@naver.com>
2021-07-09 15:41:31 +0900

    feat: pick-git 도메인이 등록된 Github Login URL을 받아오는 기능 구현

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/AuthController.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/AuthController.java
@@ -1,8 +1,25 @@
 package com.woowacourse.pickgit.authentication.presentation;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthController {
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.url.redirect-url")
+    private String redirectUrl;
+
+    @GetMapping("/authorization/github")
+    public ResponseEntity<String> loginPageUrl() {
+        String githubLoginUrl = "https://github.com/login/oauth/authorize?"
+            + "client_id=" + clientId
+            + "&redirect_url=" + redirectUrl;
+
+        return ResponseEntity.ok().body(githubLoginUrl);
+    }
 }
