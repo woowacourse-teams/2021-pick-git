de1e0efb39156c2a4ef6e74eee8e4d912d06aa32
Tanney <dhxodms89@gmail.com>
2021-07-09 17:53:56 +0900

    chore: 디렉터리 및 파일 이름 변경
    Co-authored-by: SONG WON LEE <swon3210@users.noreply.github.com>

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,7 +1,11 @@
-export const PAGE_PATH = {
+export const PAGE_URL = {
   HOME: "/",
   LOGIN: "/login",
   PROFILE: "/profile",
   ADD_POST: "/add-post",
   SEARCH: "/search",
+};
+
+export const API_URL = {
+  SELF_PROFILE: "/profiles/me",
 };
