2db3a3c6074fb9034f4e2ece727b2df52d67f70a
Daeun Lee <leede0418@likelion.org>
2021-07-13 14:41:11 +0900

    feat: 유효성 검증 로직 추가

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/post/domain/PostContent.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/post/domain/PostContent.java
@@ -13,6 +13,17 @@ public class PostContent {
     }
 
     public PostContent(String content) {
+        validate(content);
         this.content = content;
+    }
+
+    private void validate(String content) {
+        if (isOver500(content)) {
+            throw new IllegalArgumentException("F0001");
+        }
+    }
+
+    private boolean isOver500(String content) {
+        return content.length() > 500;
     }
 }
---
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/post/domain/PostContentTest.java
@@ -0,0 +1,21 @@
+package com.woowacourse.pickgit.post.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PostContentTest {
+
+    @DisplayName("게시물 내용이 500자 초과인 경우 예외가 발생한다.")
+    @Test
+    void validate_IsOver500_ThrowsException() {
+        // given
+        String content = "hi".repeat(500);
+
+        // then
+        assertThatThrownBy(() -> { new PostContent(content); })
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("F0001");
+    }
+}
