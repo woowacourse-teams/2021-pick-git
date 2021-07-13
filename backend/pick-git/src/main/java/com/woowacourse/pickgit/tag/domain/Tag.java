c9b8cffca596d2db15fbc527d9d3f81c221b9236
Jinhong <xlffm3@gmail.com>
2021-07-13 13:08:05 +0900

    refactor: 태그 도메인 생성 유효성 검사 추가 (#36)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/tag/domain/Tag.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/tag/domain/Tag.java
@@ -14,11 +14,13 @@ import javax.persistence.OneToMany;
 @Entity
 public class Tag {
 
+    private static final int MAX_TAG_LENGTH = 20;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 20)
     private String name;
 
     @OneToMany(mappedBy = "tag")
@@ -28,7 +30,16 @@ public class Tag {
     }
 
     public Tag(String name) {
+        if (isNotValidTag(name)) {
+            throw new TagFormatException();
+        }
         this.name = name;
+    }
+
+    private boolean isNotValidTag(String name) {
+        return Objects.isNull(name)
+            || name.isEmpty()
+            || name.length() > MAX_TAG_LENGTH;
     }
 
     public String getName() {
---
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/tag/domain/TagFormatException.java
@@ -0,0 +1,8 @@
+package com.woowacourse.pickgit.tag.domain;
+
+public class TagFormatException extends TagException {
+
+    public TagFormatException() {
+        super("F0003", 400);
+    }
+}
