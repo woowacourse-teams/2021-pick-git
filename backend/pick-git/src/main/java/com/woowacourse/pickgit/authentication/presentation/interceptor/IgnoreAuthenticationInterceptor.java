448166e392548330481eef8a8fb995ec8239cc4c
binghe819 <binghe819@naver.com>
2021-07-13 15:54:55 +0900

    refactor: 게시물과 프로필 조회시 로그인 상태가 아니여도 인터셉터 통과하도록 리팩토링

---
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/interceptor/IgnoreAuthenticationInterceptor.java
@@ -0,0 +1,53 @@
+package com.woowacourse.pickgit.authentication.presentation.interceptor;
+
+import com.woowacourse.pickgit.authentication.infrastructure.AuthorizationExtractor;
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class IgnoreAuthenticationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        if (isPreflightRequest(request)) {
+            return true;
+        }
+        if (isGetRequest(request)) {
+            throw new IllegalArgumentException("get 요청일 때만 가능");
+        }
+
+        String authentication = AuthorizationExtractor.extract(request);
+        if (authentication != null) {
+            request.setAttribute("authentication", authentication);
+        }
+        return true;
+    }
+
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptions(request) && hasAccessControlRequestHeaders(request) && hasAccessControlRequestMethod(request)
+            && hasOrigin(request);
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    private boolean hasAccessControlRequestHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+    }
+
+    private boolean hasAccessControlRequestMethod(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+    }
+
+    private boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Origin"));
+    }
+
+    private boolean isGetRequest(HttpServletRequest request) {
+        return request.getMethod().equals(HttpMethod.GET.name());
+    }
+}
