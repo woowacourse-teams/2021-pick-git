e87414654d9e43695483bb6b4a3548d4ec9f7415
Tanney <dhxodms89@gmail.com>
2021-07-13 23:14:37 +0900

    feat: user context 구현

---
+++ frontend/src/contexts/UserContext.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { createContext, useState } from "react";
+import useLocalStorage from "../services/hooks/@common/useLocalStorage";
+
+interface Value {
+  isLoggedIn: boolean;
+  currentUserName: string;
+  login: (accessToken: string, userName: string) => void;
+  logout: () => void;
+}
+
+const UserContext = createContext<Value>({
+  currentUserName: "",
+  isLoggedIn: false,
+  login: () => {},
+  logout: () => {},
+});
+
+export const UserContextProvider = ({ children }: HTMLElement) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentUserName, setCurrentUserName] = useState("");
+  const { setAccessToken } = useLocalStorage();
+
+  const login = (accessToken: string, userName: string) => {
+    setAccessToken(accessToken);
+    setIsLoggedIn(true);
+    setCurrentUserName(userName);
+  };
+
+  const logout = () => {
+    setAccessToken("");
+    setIsLoggedIn(false);
+  };
+
+  return <UserContext.Provider value={{ currentUserName, isLoggedIn, login, logout }}>{children}</UserContext.Provider>;
+};
+
+export default UserContext;
