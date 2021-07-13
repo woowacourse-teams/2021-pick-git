7ea5369b65b6a7899b6d80b0b6fb966d1546a74a
Tanney <dhxodms89@gmail.com>
2021-07-14 00:58:40 +0900

    feat: Provider 분리 및 UserContextProvider 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,17 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 
-import { theme, GlobalStyle } from "./App.style";
 import { PAGE_URL } from "./constants/urls";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Switch>
-          <Route path={PAGE_URL.LOGIN}>
-            <LoginPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Switch>
+        <Route path={PAGE_URL.LOGIN}>
+          <LoginPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
