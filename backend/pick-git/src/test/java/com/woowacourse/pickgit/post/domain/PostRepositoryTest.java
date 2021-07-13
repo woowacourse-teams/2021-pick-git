5d4f49ee8f8ee0beb1239f5eb8cc7ecac76209d1
Daeun Lee <leede0418@likelion.org>
2021-07-13 17:36:16 +0900

    test: Data JPA 테스트 수정

--- backend/pick-git/src/test/java/com/woowacourse/pickgit/post/domain/PostRepositoryTest.java
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/post/domain/PostRepositoryTest.java
@@ -3,6 +3,7 @@ package com.woowacourse.pickgit.post.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.woowacourse.pickgit.user.domain.User;
+import com.woowacourse.pickgit.user.domain.UserRepository;
 import com.woowacourse.pickgit.user.domain.profile.BasicProfile;
 import com.woowacourse.pickgit.user.domain.profile.GithubProfile;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,11 +11,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
 class PostRepositoryTest {
 
     private static final String USERNAME = "dani";
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private PostRepository postRepository;
@@ -57,9 +65,16 @@ class PostRepositoryTest {
         Post post = new Post(postContent, user);
 
         // when
-        Post savedPost = postRepository.save(post);
+        userRepository.save(user);
+        postRepository.save(post);
+
+        testEntityManager.flush();
+        testEntityManager.clear();
+
+        Post findPost = postRepository.findByUser(user)
+            .orElseThrow(IllegalArgumentException::new);
 
         // then
-        assertThat(savedPost.getId()).isNotNull();
+        assertThat(findPost.getId()).isNotNull();
     }
 }
