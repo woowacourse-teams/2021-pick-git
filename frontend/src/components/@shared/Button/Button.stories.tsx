41bc13f1cd05129f6c0aacf91ef27c023da3ea4a
Tanney102 <57767891+Tanney-102@users.noreply.github.com>
2021-07-14 17:22:28 +0900

    feat: Button 컴포넌트 kind type 추가(#107) (#111)
    - SquaredBlock, RoundedInline추가

--- frontend/src/components/@shared/Button/Button.stories.tsx
+++ frontend/src/components/@shared/Button/Button.stories.tsx
@@ -11,3 +11,27 @@ const Template: Story<Props> = (args) => <Button {...args}>깃들다</Button>;
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const BaemintSquaredInline = Template.bind({});
+BaemintSquaredInline.args = {
+  kind: "squaredInline",
+  backgroundColor: "#40b7b2",
+};
+
+export const BaemintSquaredBlock = Template.bind({});
+BaemintSquaredBlock.args = {
+  kind: "squaredBlock",
+  backgroundColor: "#40b7b2",
+};
+
+export const BaemintRoundedInline = Template.bind({});
+BaemintRoundedInline.args = {
+  kind: "roundedInline",
+  backgroundColor: "#40b7b2",
+};
+
+export const BaemintRoundedBlock = Template.bind({});
+BaemintRoundedBlock.args = {
+  kind: "roundedBlock",
+  backgroundColor: "#40b7b2",
+};
