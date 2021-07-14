993ff34402407b80b9f2625ddb312dd3c557f2c3
Tanney <dhxodms89@gmail.com>
2021-07-15 03:38:32 +0900

    feat: ProfileHeader 컴포넌트 구현 (#89)

---
+++ frontend/src/components/@shared/ProfileHeader/ProfileHeader.tsx
@@ -0,0 +1,63 @@
+import { useContext } from "react";
+import { ThemeContext } from "styled-components";
+
+import { Profile } from "../../../@types";
+import UserContext from "../../../contexts/UserContext";
+import Avatar from "../Avatar/Avatar";
+import Button from "../Button/Button";
+import CountIndicator from "../CountIndicator/CountIndicator";
+import { Container, Indicators } from "./ProfileHeader.style";
+
+export interface Props {
+  profile?: Profile;
+  isMyProfile: boolean;
+}
+
+const ProfileHeader = ({ profile, isMyProfile }: Props) => {
+  const { isLoggedIn } = useContext(UserContext);
+  const theme = useContext(ThemeContext);
+
+  const getButton = () => {
+    if (!isLoggedIn) {
+      return;
+    }
+
+    if (isMyProfile) {
+      return (
+        <Button type="button" kind="squaredBlock">
+          프로필 수정
+        </Button>
+      );
+    }
+
+    if (profile?.following) {
+      return (
+        <Button type="button" kind="squaredBlock" backgroundColor={theme.color.tertiaryColor}>
+          팔로우 취소
+        </Button>
+      );
+    } else {
+      return (
+        <Button type="button" kind="squaredBlock">
+          팔로우
+        </Button>
+      );
+    }
+  };
+
+  return (
+    <Container>
+      <Avatar diameter="3.75rem" fontSize="0.875rem" imageUrl={profile?.image} name={profile?.name} />
+      <div>
+        <Indicators>
+          <CountIndicator name="게시물" count={profile?.postCount ?? 0} />
+          <CountIndicator name="팔로워" count={profile?.followerCount ?? 0} />
+          <CountIndicator name="팔로잉" count={profile?.followingCount ?? 0} />
+        </Indicators>
+        {getButton()}
+      </div>
+    </Container>
+  );
+};
+
+export default ProfileHeader;
