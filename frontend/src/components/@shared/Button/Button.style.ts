fe9593d6b99cf619d15249e393fb40b318012ba8
Tanney <dhxodms89@gmail.com>
2021-07-14 16:57:43 +0900

    feat: Button 컴포넌트 kind type 추가(#107)
    - SquaredBlock, RoundedInline추가

--- frontend/src/components/@shared/Button/Button.style.ts
+++ frontend/src/components/@shared/Button/Button.style.ts
@@ -1,28 +1,40 @@
 import styled from "styled-components";
 
-const Button = styled.button`
+const Button = styled.button<React.CSSProperties>`
+  ${({ theme, backgroundColor, color }) => `
+    color: ${color ?? theme.color.white};
+    background-color: ${backgroundColor ?? theme.color.primaryColor};
+
+  `}
+
+  padding: 0.4375rem 0.875rem;
+  font-size: 0.75rem;
   text-align: center;
   transition: opacity 0.5s;
-  ${({ theme }) => `
-    color: ${theme.color.white};
-    background-color: ${theme.color.primaryColor};
 
-    :hover {
-      opacity: 0.5;
-    }
-  `}
+  :hover {
+    opacity: 0.5;
+  }
+`;
+
+export const SquaredInlineButton = styled(Button)`
+  display: inline-block;
+  border-radius: 4px;
+`;
+
+export const SquaredBlockButton = styled(Button)`
+  display: block;
+  width: 100%;
+  border-radius: 4px;
+`;
+
+export const RoundedInlineButton = styled(Button)`
+  display: inline-block;
+  border-radius: 24px;
 `;
 
 export const RoundedBlockButton = styled(Button)`
   display: block;
   width: 100%;
   border-radius: 24px;
-  padding: 0.875rem;
-`;
-
-export const SquareInlineButton = styled(Button)`
-  display: inline-block;
-  border-radius: 4px;
-  padding: 0.4375rem 0.875rem;
-  font-size: 0.75rem;
 `;
--- frontend/src/components/@shared/Button/Button.tsx
+++ frontend/src/components/@shared/Button/Button.tsx
@@ -1,15 +1,20 @@
-import { RoundedBlockButton, SquareInlineButton } from "./Button.style";
+import { SquaredInlineButton, SquaredBlockButton, RoundedInlineButton, RoundedBlockButton } from "./Button.style";
 
 export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  kind?: "roundedBlock" | "inlineSquare";
+  kind?: "squaredInline" | "squaredBlock" | "roundedInline" | "roundedBlock";
+  backgroundColor?: string;
+  color?: string;
 }
 
-const Button = ({ kind = "inlineSquare", ...props }: Props) => {
-  if (kind === "roundedBlock") {
-    return <RoundedBlockButton {...props}></RoundedBlockButton>;
-  }
+const Button = ({ kind = "squaredInline", ...props }: Props) => {
+  const button = {
+    squaredInline: <SquaredInlineButton {...props} />,
+    squaredBlock: <SquaredBlockButton {...props} />,
+    roundedInline: <RoundedInlineButton {...props} />,
+    roundedBlock: <RoundedBlockButton {...props} />,
+  };
 
-  return <SquareInlineButton {...props}></SquareInlineButton>;
+  return button[kind];
 };
 
 export default Button;
