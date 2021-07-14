559e924ada85406f4b02a2367e47c500ec850272
Tanney <dhxodms89@gmail.com>
2021-07-14 15:58:46 +0900

    fix: 누락된 url constants 추가

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,15 +1,21 @@
 export const PAGE_URL = {
   HOME: "/",
   LOGIN: "/login",
-  PROFILE: "/profile",
   ADD_POST: "/add-post",
+  EDIT_POST: "/edit-post",
   SEARCH: "/search",
+  MY_PROFILE: "/profile/me",
+  USER_PROFILE: (userName: string) => `/profile/${userName}`,
+  POSTS_WITH_TAG: (tag: string) => `/posts/${tag}`,
 };
 
 export const API_URL = {
-  SELF_PROFILE: "/profiles/me",
   AFTER_LOGIN: (code: string) => `afterlogin?code=${code}`,
   AUTH: {
     GITHUB: "/authorization/github",
   },
+  POSTS: "/posts",
+  SELF_PROFILE: "/profiles/me",
+  POSTS_LIKES: (postId: string) => `/posts/${postId}/likes`,
+  POSTS_COMMENTS: (postId: string) => `/posts/${postId}/comments`,
 };
