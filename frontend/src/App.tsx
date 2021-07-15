c3ce97361ecebbc77e110b27d296c7ba6b7b2949
Tanney <dhxodms89@gmail.com>
2021-07-16 04:10:49 +0900

    feat: 데모를 위해 항상 로그인 상태를 유지하도록 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { useContext, useEffect } from "react";
 
 import { PAGE_URL } from "./constants/urls";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -6,8 +7,15 @@ import NavigationHeader from "./components/@layout/NavigationHeader/NavigationHe
 import HomeFeedPage from "./pages/HomeFeedPage/HomeFeedPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { Page } from "./components/@styled/layout";
+import UserContext from "./contexts/UserContext";
 
 const App = () => {
+  const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    login("testToken", "tanney-102");
+  }, []);
+
   return (
     <BrowserRouter>
       <Switch>
