fc46a1a51179877d971fbbd831dfd683be00a7f8
Tanney <dhxodms89@gmail.com>
2021-07-16 03:08:20 +0900

    feat: svg icon 추가

--- frontend/src/assets/icons/index.tsx
+++ frontend/src/assets/icons/index.tsx
@@ -19,3 +19,8 @@ export { ReactComponent as GithubDarkIcon } from "./github-brands.svg";
 export { ReactComponent as LocationIcon } from "./map-marker-alt-solid.svg";
 export { ReactComponent as WebsiteLinkIcon } from "./link-solid.svg";
 export { ReactComponent as TwitterIcon } from "./twitter-brands.svg";
+export { ReactComponent as StarIcon } from "./star.svg";
+export { ReactComponent as ClockIcon } from "./clock.svg";
+export { ReactComponent as PrIcon } from "./pr.svg";
+export { ReactComponent as IssueIcon } from "./issue.svg";
+export { ReactComponent as BookIcon } from "./book.svg";
