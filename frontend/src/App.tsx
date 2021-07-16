ade134666149cd4854e649b771283f01431492cd
Tanney102 <57767891+Tanney-102@users.noreply.github.com>
2021-07-16 18:56:27 +0900

    [chore] api mocking 관련 코드 수정 (#140)
    * feat: 무한스크롤 구현 전임을 감안하여 posts 조회시 page, limit mock
    
    * feat: login mock 제거
    
    * chore: 배포 환경에 따라 api proxy여부 분기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import { useContext, useEffect } from "react";
 
 import { PAGE_URL } from "./constants/urls";
 import LoginPage from "./pages/LoginPage/LoginPage";
@@ -7,21 +6,14 @@ import NavigationHeader from "./components/@layout/NavigationHeader/NavigationHe
 import HomeFeedPage from "./pages/HomeFeedPage/HomeFeedPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { Page } from "./components/@styled/layout";
-import UserContext from "./contexts/UserContext";
 import AuthLoginProcessingPage from "./pages/AuthLoginProcessingPage/AuthLoginProcessingPage";
 
 const App = () => {
-  const { login } = useContext(UserContext);
-
-  useEffect(() => {
-    login("testToken", "tanney-102");
-  }, []);
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={[PAGE_URL.HOME, PAGE_URL.PROFILE]}>
-          <NavigationHeader isLoggedIn={false} />
+        <Route exact path={[PAGE_URL.HOME, PAGE_URL.PROFILE, PAGE_URL.MY_PROFILE]}>
+          <NavigationHeader />
         </Route>
       </Switch>
       <Page>
--- frontend/src/services/requests/index.ts
+++ frontend/src/services/requests/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "http://devapi.pickgit.p-e.kr:8080/api" : "/api";
 
 export * from "./profile";
 export * from "./account";
