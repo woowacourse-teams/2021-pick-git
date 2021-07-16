81b5fd5173f5a4254103eae72eff43e88ba44cac
Jinhong <xlffm3@gmail.com>
2021-07-16 10:13:48 +0900

    fix: DataJpaTest에 Date 관련 테스트 설정 추가 (#123)

--- backend/pick-git/src/test/java/com/woowacourse/pickgit/post/domain/PostRepositoryTest.java
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/post/domain/PostRepositoryTest.java
@@ -2,6 +2,7 @@ package com.woowacourse.pickgit.post.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.woowacourse.pickgit.TestJpaConfiguration;
 import com.woowacourse.pickgit.post.domain.comment.Comment;
 import com.woowacourse.pickgit.post.domain.comment.Comments;
 import com.woowacourse.pickgit.post.domain.content.Image;
@@ -11,6 +12,7 @@ import com.woowacourse.pickgit.tag.domain.TagRepository;
 import com.woowacourse.pickgit.user.domain.User;
 import com.woowacourse.pickgit.user.domain.profile.BasicProfile;
 import com.woowacourse.pickgit.user.domain.profile.GithubProfile;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,7 +22,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 
+@Import({TestJpaConfiguration.class})
 @DataJpaTest
 class PostRepositoryTest {
 
@@ -80,6 +84,20 @@ class PostRepositoryTest {
 
         // then
         assertThat(savedPost.getId()).isNotNull();
+    }
+
+    @DisplayName("게시글을 저장하면 자동으로 생성 날짜가 주입된다.")
+    @Test
+    void save_SavedPostWithCreatedDate_Success() {
+        // given
+        Images images = new Images(List.of(new Image(image)));
+        Post post = new Post(postContent, images, githubRepoUrl, user);
+
+        // when
+        Post savedPost = postRepository.save(post);
+
+        assertThat(savedPost.getCreatedAt()).isNotNull();
+        assertThat(savedPost.getCreatedAt()).isBefore(LocalDateTime.now());
     }
 
     @DisplayName("게시글을 저장할 때 태그도 함께 영속화된다.")
