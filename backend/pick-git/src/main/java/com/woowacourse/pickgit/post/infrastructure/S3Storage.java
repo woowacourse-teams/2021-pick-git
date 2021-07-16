f9f1521ed871adecdf19749595807079abbc8f1c
Minsung <sns2831@gmail.com>
2021-07-17 01:55:00 +0900

    fix: 응답 파싱 에러 수정

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/post/infrastructure/S3Storage.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/post/infrastructure/S3Storage.java
@@ -1,11 +1,17 @@
 package com.woowacourse.pickgit.post.infrastructure;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.xml.bind.v2.model.core.TypeRef;
+import com.woowacourse.pickgit.exception.platform.PlatformHttpErrorException;
 import com.woowacourse.pickgit.post.presentation.PickGitStorage;
 import java.io.File;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.LinkedMultiValueMap;
@@ -17,6 +23,12 @@ import org.springframework.web.client.RestTemplate;
 public class S3Storage implements PickGitStorage {
     private static final String MULTIPART_KEY = "files";
 
+    private final ObjectMapper objectMapper;
+
+    public S3Storage(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Value("${storage.pickgit.s3proxy}")
     private String s3ProxyUrl;
 
@@ -25,10 +37,17 @@ public class S3Storage implements PickGitStorage {
     public List<String> store(List<File> files, String userName) {
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<List> listResponseEntity = restTemplate
-            .postForEntity(s3ProxyUrl, createBody(files, userName), List.class);
+        ResponseEntity<String> stringResponseEntity = restTemplate
+            .postForEntity(s3ProxyUrl, createBody(files, userName), String.class);
 
-        return (List<String>) listResponseEntity.getBody();
+        String body = stringResponseEntity.getBody();
+
+        try {
+            return objectMapper.readValue(body, new TypeReference<>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new PlatformHttpErrorException();
+        }
     }
 
     private MultiValueMap<String, Object> createBody(
