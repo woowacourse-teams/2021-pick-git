dffe40c3ed351138319f37da1c44daa654f914a7
binghe819 <binghe819@naver.com>
2021-07-17 00:21:41 +0900

    refactor: 로그인후 반환할 때 토큰과 함께 유저 네임도 보내도록 수정

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/dto/OAuthTokenResponse.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/dto/OAuthTokenResponse.java
@@ -3,12 +3,14 @@ package com.woowacourse.pickgit.authentication.presentation.dto;
 public class OAuthTokenResponse {
 
     private String token;
+    private String username;
 
     public OAuthTokenResponse() {
     }
 
-    public OAuthTokenResponse(String token) {
+    public OAuthTokenResponse(String token, String username) {
         this.token = token;
+        this.username = username;
     }
 
     public String getToken() {
@@ -17,5 +19,13 @@ public class OAuthTokenResponse {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
