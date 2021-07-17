630668afcb6e0be5bc948c7d6186788b8f3270d0
SONG WON LEE <swon3210@gmail.com>
2021-07-18 00:07:13 +0900

    feat: 페이지 컴포넌트에서 데이터를 fetch 받도록 수정

--- frontend/src/components/Feed/Feed.tsx
+++ frontend/src/components/Feed/Feed.tsx
@@ -2,20 +2,24 @@ import axios from "axios";
 import { useContext } from "react";
 
 import UserContext from "../../contexts/UserContext";
+import { useState } from "react";
 import { Post } from "../../@types";
 import { LIMIT } from "../../constants/limits";
 import { FAILURE_MESSAGE } from "../../constants/message";
 import useFeed from "../../services/hooks/useFeed";
+import InfiniteScrollContainer from "../@shared/InfiniteScrollContainer/InfiniteScrollContainer";
 import PostItem from "../@shared/PostItem/PostItem";
 import { Container, PostItemWrapper } from "./Feed.style";
-import { useQueryClient } from "react-query";
+import { UseInfiniteQueryResult, useQueryClient } from "react-query";
 import { QUERY } from "../../constants/queries";
 
-const Feed = () => {
+const Feed = ({ queryResult }: { queryResult: UseInfiniteQueryResult<Post[], unknown> }) => {
   const { logout } = useContext(UserContext);
   const queryClient = useQueryClient();
-  const { posts, isLoading, error, commentValue, setCommentValue, deletePostLike, addPostLike, setPosts, addComment } =
-    useFeed();
+  const { data, isLoading, error, fetchNextPage } = queryResult;
+  const { commentValue, setCommentValue, deletePostLike, addPostLike, setPosts, addComment } = useFeed();
+
+  const allPosts = data?.pages?.reduce((acc, postPage) => acc.concat(postPage), []);
 
   const handleCommentValueChange: React.ChangeEventHandler<HTMLTextAreaElement> = ({ target: { value } }) => {
     if (value.length > LIMIT.COMMENT_LENGTH) {
@@ -35,11 +39,11 @@ const Feed = () => {
   };
 
   const handlePostLike = (postId: string) => {
-    if (!posts) {
+    if (!allPosts) {
       return;
     }
 
-    const newPosts = [...posts];
+    const newPosts = [...allPosts];
     const targetPost = newPosts.find((post) => post.postId === postId);
 
     if (!targetPost) {
@@ -58,6 +62,11 @@ const Feed = () => {
       targetPost.isLiked = true;
       setPosts(newPosts);
     }
+  };
+
+  const handlePostsEndIntersect = () => {
+    fetchNextPage();
+    // setPageIndex((state) => state + 1);
   };
 
   if (error) {
@@ -79,31 +88,33 @@ const Feed = () => {
 
   return (
     <Container>
-      {posts?.map((post) => (
-        <PostItemWrapper key={post.postId}>
-          <PostItem
-            authorName={post.authorName}
-            authorGithubUrl={post.githubRepoUrl}
-            authorImageUrl={post.profileImageUrl}
-            imageUrls={post.imageUrls}
-            commenterImageUrl={
-              "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-            }
-            createdAt={post.createdAt}
-            comments={post.comments}
-            content={post.content}
-            isEditable={true}
-            isLiked={post.isLiked}
-            likeCount={post.likesCount}
-            tags={post.tags}
-            commentValue={commentValue}
-            onCommentValueChange={handleCommentValueChange}
-            onCommentValueSave={() => handleCommentValueSave(post.postId)}
-            onCommentLike={handleCommentLike}
-            onPostLike={() => handlePostLike(post.postId)}
-          />
-        </PostItemWrapper>
-      ))}
+      <InfiniteScrollContainer onIntersect={handlePostsEndIntersect}>
+        {allPosts?.map((post) => (
+          <PostItemWrapper key={post.postId}>
+            <PostItem
+              authorName={post.authorName}
+              authorGithubUrl={post.githubRepoUrl}
+              authorImageUrl={post.profileImageUrl}
+              imageUrls={post.imageUrls}
+              commenterImageUrl={
+                "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+              }
+              createdAt={post.createdAt}
+              comments={post.comments}
+              content={post.content}
+              isEditable={true}
+              isLiked={post.isLiked}
+              likeCount={post.likesCount}
+              tags={post.tags}
+              commentValue={commentValue}
+              onCommentValueChange={handleCommentValueChange}
+              onCommentValueSave={() => handleCommentValueSave(post.postId)}
+              onCommentLike={handleCommentLike}
+              onPostLike={() => handlePostLike(post.postId)}
+            />
+          </PostItemWrapper>
+        ))}
+      </InfiniteScrollContainer>
     </Container>
   );
 };
--- frontend/src/pages/HomeFeedPage/HomeFeedPage.tsx
+++ frontend/src/pages/HomeFeedPage/HomeFeedPage.tsx
@@ -1,10 +1,13 @@
 import { Container } from "./HomeFeedPage.style";
 import Feed from "../../components/Feed/Feed";
+import { useHomeFeedPostsQuery } from "../../services/queries";
 
 const HomeFeedPage = () => {
+  const queryResult = useHomeFeedPostsQuery();
+
   return (
     <Container>
-      <Feed />
+      <Feed queryResult={queryResult} />
     </Container>
   );
 };
