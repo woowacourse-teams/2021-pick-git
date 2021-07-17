9e24b3132d4647a345c9718668c763d38a4495e7
binghe819 <binghe819@naver.com>
2021-07-17 20:46:30 +0900

    refactor: 상수화

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/AuthenticationPrincipalArgumentResolver.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/AuthenticationPrincipalArgumentResolver.java
@@ -2,8 +2,10 @@ package com.woowacourse.pickgit.authentication.presentation;
 
 import com.woowacourse.pickgit.authentication.application.OAuthService;
 import com.woowacourse.pickgit.authentication.domain.Authenticated;
+import com.woowacourse.pickgit.authentication.presentation.interceptor.AuthHeader;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -27,7 +29,7 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
         NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        String authentication = (String) request.getAttribute("authentication");
+        String authentication = (String) request.getAttribute(AuthHeader.AUTHENTICATION);
 
         return oAuthService.findRequestUserByToken(authentication);
     }
--- backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/interceptor/IgnoreAuthenticationInterceptor.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/authentication/presentation/interceptor/IgnoreAuthenticationInterceptor.java
@@ -6,6 +6,7 @@ import com.woowacourse.pickgit.exception.authentication.InvalidTokenException;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -26,10 +27,10 @@ public class IgnoreAuthenticationInterceptor implements HandlerInterceptor {
         }
 
         String authentication = AuthorizationExtractor.extract(request);
-        if (authentication != null && !oAuthService.validateToken(authentication)) {
+        if (Objects.nonNull(authentication) && !oAuthService.validateToken(authentication)) {
             throw new InvalidTokenException();
         }
-        request.setAttribute("authentication", authentication);
+        request.setAttribute(AuthHeader.AUTHENTICATION, authentication);
         return true;
     }
 
@@ -45,14 +46,14 @@ public class IgnoreAuthenticationInterceptor implements HandlerInterceptor {
     }
 
     private boolean hasAccessControlRequestHeaders(HttpServletRequest request) {
-        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+        return Objects.nonNull(request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS));
     }
 
     private boolean hasAccessControlRequestMethod(HttpServletRequest request) {
-        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+        return Objects.nonNull(request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD));
     }
 
     private boolean hasOrigin(HttpServletRequest request) {
-        return Objects.nonNull(request.getHeader("Origin"));
+        return Objects.nonNull(request.getHeader(HttpHeaders.ORIGIN));
     }
 }
