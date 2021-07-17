155fabb65105b084fe2306d686117817d9d983cb
SONG WON LEE <swon3210@gmail.com>
2021-07-17 21:55:42 +0900

    [#173] Feature/loader and infinite scroll container (#175)
    * feat: Loader 컴포넌트 구현
    
    * feat: InfiniteScrollContainer 컴포넌트 구현

---
+++ frontend/src/components/@shared/InfiniteScrollContainer/InfiniteScrollContainer.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div<React.CSSProperties>``;
+
+export const ContentWrapper = styled.div`
+  margin-bottom: 2rem;
+`;
+
+export const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
