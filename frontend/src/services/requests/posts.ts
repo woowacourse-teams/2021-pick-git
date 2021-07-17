cdca660ba6251a00c2b46b3cf759618f2095d5de
SONG WON LEE <swon3210@gmail.com>
2021-07-17 22:04:07 +0900

    feat: requestAddPost 요청 함수 구현

--- frontend/src/services/requests/posts.ts
+++ frontend/src/services/requests/posts.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-import { Post } from "../../@types";
+import { Post, PostAddFormData } from "../../@types";
+import { LIMIT } from "../../constants/limits";
 import { API_URL } from "../../constants/urls";
 
-export const requestGetHomeFeedPosts = async (accessToken: string | null) => {
+export const requestGetHomeFeedPosts = async (pageIndex: number, accessToken: string | null) => {
   const config = accessToken
     ? {
         headers: {
@@ -11,7 +12,8 @@ export const requestGetHomeFeedPosts = async (accessToken: string | null) => {
         },
       }
     : {};
-  const response = await axios.get<Post[]>(API_URL.POSTS(0, 20), config);
+
+  const response = await axios.get<Post[]>(API_URL.POSTS(pageIndex, LIMIT.FEED_COUNT_PER_FETCH), config);
 
   return response.data;
 };
@@ -65,4 +67,25 @@ export const requestDeletePostLike = async (postId: string, accessToken: string 
   });
 
   return response.data;
+};
+
+export const requestAddPost = async (
+  { files, githubRepositoryName, tags, content }: PostAddFormData,
+  accessToken: string | null
+) => {
+  if (!accessToken) {
+    throw Error("no accessToken");
+  }
+
+  const formData = new FormData();
+  files.forEach((file) => formData.append("images", file));
+  formData.append("githubRepoUrl", `https://github.com/swon3210/${githubRepositoryName}`);
+  formData.append("tags", JSON.stringify(tags));
+  formData.append("content", content);
+
+  await axios.post(API_URL.ADD_POSTS, formData, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
 };
