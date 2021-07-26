ae35525ae97401212c87b35eb2f983ba6568296c
Tanney <dhxodms89@gmail.com>
2021-07-26 17:33:11 +0900

    feat: TextEditor 컴포넌트에 indicator 추가(#226)

--- frontend/src/components/@shared/TextEditor/TextEditor.style.ts
+++ frontend/src/components/@shared/TextEditor/TextEditor.style.ts
@@ -1,16 +1,33 @@
 import styled from "styled-components";
 
-const TextArea = styled.textarea<React.CSSProperties>`
-  ${({ width, height, minHeight, fontSize }) => `
-    width: ${width ?? "100%"};
-    min-height: ${minHeight ?? "fit-content"};
-    height: ${height ?? "fit-content"};
-    font-size: ${fontSize ?? "1rem"};
-  `}
+export const Container = styled.div<React.CSSProperties>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 
+  ${({ height, minHeight }) => `
+    height: ${height === "" ? minHeight : `calc(${height} + 1.5rem)`};
+    min-height: ${minHeight};
+  `}
+`;
+
+export const TextArea = styled.textarea<React.CSSProperties>`
+  ${({ width, height, fontSize }) => `
+    width: ${width ?? "100%"};
+    height: ${height === "" ? "100%" : height};
+    font-size: ${fontSize ?? "1rem"};
+`}
+
+  height: 100%;
   border: none;
   outline: none;
   background-color: transparent;
 `;
 
-export default TextArea;
+export const TextLengthIndicator = styled.div<React.CSSProperties>`
+  margin-top: 0.5rem;
+  color: ${({ theme }) => theme.color.lighterTextColor};
+  font-size: 0.8rem;
+  float: right;
+  height: 1rem;
+`;
