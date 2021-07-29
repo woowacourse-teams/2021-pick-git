d9e1873a5bd04cd5fda6b79297d0504ebb54fe68
Daeun Lee <leede0418@likelion.org>
2021-07-30 01:35:27 +0900

    test: 단위 테스트 추가 (#253)

--- backend/pick-git/src/test/java/com/woowacourse/pickgit/unit/user/infrastructure/GithubContributionExtractorTest.java
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/unit/user/infrastructure/GithubContributionExtractorTest.java
@@ -1,29 +1,32 @@
 package com.woowacourse.pickgit.unit.user.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowacourse.pickgit.common.mockapi.MockContributionApiRequester;
+import com.woowacourse.pickgit.exception.user.ContributionParseException;
 import com.woowacourse.pickgit.user.domain.PlatformContributionExtractor;
 import com.woowacourse.pickgit.user.domain.dto.CountResponseDto;
 import com.woowacourse.pickgit.user.domain.dto.StarResponseDto;
 import com.woowacourse.pickgit.user.infrastructure.extractor.GithubContributionExtractor;
+import com.woowacourse.pickgit.user.infrastructure.requester.PlatformContributionApiRequester;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 public class GithubContributionExtractorTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String apiUrlFormatForStar = "https://api.github.com/search/repositories?q=user:%s stars:>=1";
+    private final String apiUrlFormatForCount = "https://api.github.com/search/";
 
     private PlatformContributionExtractor platformContributionExtractor;
 
     @BeforeEach
     void setUp() {
-        String apiUrlFormatForStar = "https://api.github.com/search/repositories?q=user:%s stars:>=1";
-        String apiUrlFormatForCount = "https://api.github.com/search/";
-
         platformContributionExtractor = new GithubContributionExtractor(
             objectMapper,
             new MockContributionApiRequester(),
@@ -45,6 +48,26 @@ public class GithubContributionExtractorTest {
             .isEqualTo(11);
     }
 
+    @DisplayName("JSON key 값이 다른 경우 스타 개수를 추출할 수 없다. - 500 예외")
+    @Test
+    void extractStars_InvalidJsonKeyInCaseOfStars_500Exception() {
+        // given
+        platformContributionExtractor = new GithubContributionExtractor(
+            objectMapper,
+            new MockContributionApiErrorRequester(),
+            apiUrlFormatForStar,
+            apiUrlFormatForCount
+        );
+
+        // when
+        assertThatThrownBy(() -> {
+            platformContributionExtractor.extractStars("testUser");
+        }).isInstanceOf(ContributionParseException.class)
+            .hasFieldOrPropertyWithValue("errorCode", "V0001")
+            .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.INTERNAL_SERVER_ERROR)
+            .hasMessage("활동 통계를 조회할 수 없습니다.");
+    }
+
     @DisplayName("커밋 개수를 추출한다.")
     @Test
     void extractCount_Commits_Success() {
@@ -56,6 +79,26 @@ public class GithubContributionExtractorTest {
         assertThat(commits.getCount()).isEqualTo(48);
     }
 
+    @DisplayName("JSON key 값이 다른 경우 커밋 개수를 추출할 수 없다. - 500 예외")
+    @Test
+    void extractCount_InvalidJsonKeyInCaseOfCommits_500Exception() {
+        // given
+        platformContributionExtractor = new GithubContributionExtractor(
+            objectMapper,
+            new MockContributionApiErrorRequester(),
+            apiUrlFormatForStar,
+            apiUrlFormatForCount
+        );
+
+        // when
+        assertThatThrownBy(() -> {
+            platformContributionExtractor.extractCount("/commits?q=committer:%s", "testUser");
+        }).isInstanceOf(ContributionParseException.class)
+            .hasFieldOrPropertyWithValue("errorCode", "V0001")
+            .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.INTERNAL_SERVER_ERROR)
+            .hasMessage("활동 통계를 조회할 수 없습니다.");
+    }
+
     @DisplayName("PR 개수를 추출한다.")
     @Test
     void extractCount_PRs_Success() {
@@ -65,6 +108,26 @@ public class GithubContributionExtractorTest {
 
         // then
         assertThat(prs.getCount()).isEqualTo(48);
+    }
+
+    @DisplayName("JSON key 값이 다른 경우 PR 개수를 추출할 수 없다. - 500 예외")
+    @Test
+    void extractCount_InvalidJsonKeyInCaseOfPRs_500Exception() {
+        // given
+        platformContributionExtractor = new GithubContributionExtractor(
+            objectMapper,
+            new MockContributionApiErrorRequester(),
+            apiUrlFormatForStar,
+            apiUrlFormatForCount
+        );
+
+        // when
+        assertThatThrownBy(() -> {
+            platformContributionExtractor.extractCount("/issues?q=author:%s type:pr", "testUser");
+        }).isInstanceOf(ContributionParseException.class)
+            .hasFieldOrPropertyWithValue("errorCode", "V0001")
+            .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.INTERNAL_SERVER_ERROR)
+            .hasMessage("활동 통계를 조회할 수 없습니다.");
     }
 
     @DisplayName("이슈 개수를 추출한다.")
@@ -79,6 +142,26 @@ public class GithubContributionExtractorTest {
         assertThat(issues.getCount()).isEqualTo(48);
     }
 
+    @DisplayName("JSON key 값이 다른 경우 이슈 개수를 추출할 수 없다. - 500 예외")
+    @Test
+    void extractCount_InvalidJsonKeyInCaseOfIssues_500Exception() {
+        // given
+        platformContributionExtractor = new GithubContributionExtractor(
+            objectMapper,
+            new MockContributionApiErrorRequester(),
+            apiUrlFormatForStar,
+            apiUrlFormatForCount
+        );
+
+        // when
+        assertThatThrownBy(() -> {
+            platformContributionExtractor.extractCount("/issues?q=author:%s type:issue", "testUser");
+        }).isInstanceOf(ContributionParseException.class)
+            .hasFieldOrPropertyWithValue("errorCode", "V0001")
+            .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.INTERNAL_SERVER_ERROR)
+            .hasMessage("활동 통계를 조회할 수 없습니다.");
+    }
+
     @DisplayName("퍼블릭 레포지토리 개수를 추출한다.")
     @Test
     void extractCount_Repos_Success() {
@@ -89,5 +172,38 @@ public class GithubContributionExtractorTest {
 
         // then
         assertThat(repos.getCount()).isEqualTo(48);
+    }
+
+    @DisplayName("JSON key 값이 다른 경우 퍼블릭 레포지토리 개수를 추출할 수 없다. - 500 예외")
+    @Test
+    void extractCount_InvalidJsonKeyInCaseOfRepos_500Exception() {
+        // given
+        platformContributionExtractor = new GithubContributionExtractor(
+            objectMapper,
+            new MockContributionApiErrorRequester(),
+            apiUrlFormatForStar,
+            apiUrlFormatForCount
+        );
+
+        // when
+        assertThatThrownBy(() -> {
+            platformContributionExtractor.extractCount("/repositories?q=user:%s is:public", "testUser");
+        }).isInstanceOf(ContributionParseException.class)
+            .hasFieldOrPropertyWithValue("errorCode", "V0001")
+            .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.INTERNAL_SERVER_ERROR)
+            .hasMessage("활동 통계를 조회할 수 없습니다.");
+    }
+
+    private static class MockContributionApiErrorRequester
+        implements PlatformContributionApiRequester
+    {
+
+        @Override
+        public String request(String url) {
+            if (url.contains("stars")) {
+                return "[{\"stargazers\": \"5\"}, {\"stargazers\": \"6\"}]";
+            }
+            return "{\"total\": \"48\"}";
+        }
     }
 }
