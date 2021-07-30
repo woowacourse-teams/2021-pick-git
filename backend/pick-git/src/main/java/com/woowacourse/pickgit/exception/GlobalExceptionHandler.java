14b4a5d6f3666059e7017cc8f8aed7922a07b6f3
Jinhong <xlffm3@gmail.com>
2021-07-30 18:11:15 +0900

    [#236] 로깅 기능 세팅 (#237)
    * chore: 활성 프로필에 따른 logback 환경 설정 (#236)
    
    * feat: 전역 예외 어드바이저에 로거 추가 및 DB / 런타임 예외 바인딩 (#236)
    
    * chore: s3proxy directory logback 설정 추가 (#236)
    
    * feat: s3proxy 예외 어드바이저 추가 (#236)
    
    * refactor: applicationException 핸들러 어드바이저 메서드명 수정 (#236)
    
    * refactor: 백엔드 프로젝트 로깅 전략 수정 (#236)
    
    - 테스트 로깅 전략 콘솔로 수정
    - DB 관련 로거 종류 프로필별 재정의
    - 부모 로거가 찍히지 않도록 additivity false 추가
    - 글로벌 예외 어드바이저에 로깅 양식 정의
    
    * chore: logback-access 설정 추가 및 프로필별 설정 구성 (#236)
    
    * chore: s3proxy 프로젝트 logback-access 설정 추가 (#236)
    
    * refactor: s3proxy, backend 프로젝트 log 포맷 변경 (#236)
    
    * chore: backend logger DB, Access 등 용도별 분리 (#236)
    
    * chore: s3proxy logger DB, Access 등 용도별 분리 (#236)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/GlobalExceptionHandler.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.woowacourse.pickgit.exception;
 import static java.util.Objects.requireNonNull;
 
 import com.woowacourse.pickgit.exception.dto.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,24 +12,62 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    private static final String LOG_FORMAT = "Class : {}, Code : {}, Message : {}";
+    private static final String INTERNAL_SERVER_ERROR_CODE = "S0001";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> methodArgumentNotValidException(
-        MethodArgumentNotValidException e) {
-        ApiErrorResponse exceptionResponse =
-            new ApiErrorResponse(requireNonNull(e.getFieldError()).getDefaultMessage());
-
+        MethodArgumentNotValidException e
+    ) {
+        String errorCode = requireNonNull(e.getFieldError())
+            .getDefaultMessage();
+        ApiErrorResponse exceptionResponse = new ApiErrorResponse(errorCode);
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), errorCode, "@Valid");
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST.value())
             .body(exceptionResponse);
     }
 
     @ExceptionHandler(ApplicationException.class)
-    public ResponseEntity<ApiErrorResponse> authenticationException(
-        ApplicationException e) {
+    public ResponseEntity<ApiErrorResponse> applicationException(ApplicationException e) {
+        String errorCode = e.getErrorCode();
+        log.warn(
+            LOG_FORMAT,
+            e.getClass().getSimpleName(),
+            errorCode,
+            e.getMessage()
+        );
         return ResponseEntity
-            .status(e.getHttpStatus().value())
-            .body(new ApiErrorResponse(e.getErrorCode()));
+            .status(e.getHttpStatus())
+            .body(new ApiErrorResponse(errorCode));
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ApiErrorResponse> dataAccessException(DataAccessException e) {
+        log.error(
+            LOG_FORMAT,
+            e.getClass().getSimpleName(),
+            INTERNAL_SERVER_ERROR_CODE,
+            e.getMessage()
+        );
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiErrorResponse(INTERNAL_SERVER_ERROR_CODE));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiErrorResponse> runtimeException(RuntimeException e) {
+        log.error(
+            LOG_FORMAT,
+            e.getClass().getSimpleName(),
+            INTERNAL_SERVER_ERROR_CODE,
+            e.getMessage()
+        );
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ApiErrorResponse(INTERNAL_SERVER_ERROR_CODE));
     }
 }
--- s3-proxy/s3-proxy/src/main/java/com/woowacourse/s3_proxy/exception/ExceptionAdvice.java
+++ s3-proxy/s3-proxy/src/main/java/com/woowacourse/s3_proxy/exception/ExceptionAdvice.java
@@ -1,20 +1,44 @@
 package com.woowacourse.s3_proxy.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ExceptionAdvice {
+
+    private static final String LOG_FORMAT= "Class : {}, Code : {}, Message : {}";
+    private static final String INTERNAL_SERVER_ERROR_CODE = "S0001";
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ExceptionDto> applicationException(
-        ApplicationException applicationException
+        ApplicationException e
     ) {
-        return ResponseEntity.status(applicationException.getHttpStatus())
-            .body(
-                new ExceptionDto(applicationException.getErrorCode())
-            );
+        String errorCode = e.getErrorCode();
+        log.warn(
+            LOG_FORMAT,
+            e.getClass().getSimpleName(),
+            errorCode,
+            e.getMessage()
+        );
+        return ResponseEntity.status(e.getHttpStatus())
+            .body(new ExceptionDto(errorCode));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionDto> runtimeException(RuntimeException e) {
+        log.error(
+            LOG_FORMAT,
+            e.getClass().getSimpleName(),
+            INTERNAL_SERVER_ERROR_CODE,
+            e.getMessage()
+        );
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ExceptionDto(INTERNAL_SERVER_ERROR_CODE));
     }
 
     public static class ExceptionDto {
