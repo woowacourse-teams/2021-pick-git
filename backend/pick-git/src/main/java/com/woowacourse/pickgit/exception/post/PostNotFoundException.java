f702386a3a842889da7835ec2c36a858ada27406
DAEUN LEE <leede0418@gmail.com>
2021-08-01 21:47:02 +0900

    [#295] 게시물 수정 및 삭제 DB 버그 해결 (#300)
    * refactor: findPostByUser -> findPostByIdAndUser 변경 (#295)
    
    * test: Data JPA 테스트 추가 (#295)
    
    * refactor: 생성자 수정 (#295)
    
    * test: 예외 발생 -> Optional.empty() 검증 변경 (#295)
    
    * refactor: 불필요한 코드 삭제 (#295)
    
    * refactor: 해당하는 사용자의 게시물이 맞는지 확인하는 과정 수정 (#295)
    
    - DB에서 처리 -> 비즈니스 로직에서 처리로 변경
    
    * refactor: 불필요한 코드 삭제 (#295)
    
    * test: 불필요한 import 삭제 (#295)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/post/PostNotFoundException.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/post/PostNotFoundException.java
@@ -4,11 +4,10 @@ import org.springframework.http.HttpStatus;
 
 public class PostNotFoundException extends PostException {
 
-    public PostNotFoundException(
-        String errorCode,
-        HttpStatus httpStatus,
-        String message)
-    {
-        super(errorCode, httpStatus, message);
+    private static final String CODE = "P0002";
+    private static final String MESSAGE = "해당하는 게시물을 찾을 수 없습니다.";
+
+    public PostNotFoundException() {
+        super(CODE, HttpStatus.INTERNAL_SERVER_ERROR, MESSAGE);
     }
 }
