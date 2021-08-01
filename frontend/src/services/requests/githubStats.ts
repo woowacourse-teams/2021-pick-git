b30809e3f4b6d3c1190a2415cfa9dfd36eb4bbf6
Tanney102 <57767891+Tanney-102@users.noreply.github.com>
2021-08-02 02:54:32 +0900

    [#310] 팀데모 결과 반영 - 프로필 페이지 (#311)
    * fix: api url 오타 수정
    
    * fix: 누락된 key prop 추가
    
    * fix: 깃헙통계 기능 임시 api로 수정
    
    * 프로필 페이지 하단 레이아웃 버그 수정

--- frontend/src/services/requests/githubStats.ts
+++ frontend/src/services/requests/githubStats.ts
@@ -3,7 +3,8 @@ import { GithubStats } from "../../@types";
 import { API_URL } from "../../constants/urls";
 
 export const requestGetGithubStats = async (username: string) => {
-  const response = await axios.get<GithubStats>(API_URL.GITHUB_STATS(username));
+  // const response = await axios.get<GithubStats>(API_URL.GITHUB_STATS(username));
+  const response = await axios.get<GithubStats>(`https://parse-github-stats.herokuapp.com/github-stats/${username}`);
 
   return response.data;
 };
