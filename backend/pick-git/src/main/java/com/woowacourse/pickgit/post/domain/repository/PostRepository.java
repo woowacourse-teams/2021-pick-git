dadde7dd28114f764b18100b13397f86becbb1ad
Minsung Son <sns2831@gmail.com>
2021-08-04 16:24:08 +0900

    feat: tag 기반 post 검색 repository 구현 (#337)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/post/domain/repository/PostRepository.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/post/domain/repository/PostRepository.java
@@ -3,7 +3,6 @@ package com.woowacourse.pickgit.post.domain.repository;
 import com.woowacourse.pickgit.post.domain.Post;
 import com.woowacourse.pickgit.user.domain.User;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +15,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("select p from Post p where p.user = :user order by p.createdAt desc")
     List<Post> findAllPostsByUser(@Param("user") User user, Pageable pageable);
+
+    @Query("select distinct p from PostTag pt inner join pt.post p where pt.tag.name in :tagName")
+    List<Post> findAllPostsByTagNames(@Param("tagName") List<String> tagName, Pageable pageable);
 }
--- backend/pick-git/src/test/java/com/woowacourse/pickgit/unit/post/domain/PostRepositoryTest.java
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/unit/post/domain/PostRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.woowacourse.pickgit.unit.post.domain;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.woowacourse.pickgit.common.factory.UserFactory;
 import com.woowacourse.pickgit.config.JpaTestConfiguration;
+import com.woowacourse.pickgit.post.application.PostDtoAssembler;
+import com.woowacourse.pickgit.post.application.dto.response.PostResponseDto;
 import com.woowacourse.pickgit.post.domain.Post;
 import com.woowacourse.pickgit.post.domain.comment.Comment;
 import com.woowacourse.pickgit.post.domain.repository.PostRepository;
@@ -15,18 +19,24 @@ import com.woowacourse.pickgit.user.domain.UserRepository;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.data.domain.PageRequest;
 
 @Import(JpaTestConfiguration.class)
 @DataJpaTest
 class PostRepositoryTest {
-    
+
     @Autowired
     private UserRepository userRepository;
 
@@ -159,8 +169,91 @@ class PostRepositoryTest {
         assertThat(comments).hasSize(1);
     }
 
+    @DisplayName("Tag를 기반으로 Post를 불러온다.")
+    @ParameterizedTest
+    @MethodSource("getPostsAndTagsForSearchPostByTagName")
+    void findAllPostsByTagNames_findAllPostsByTagNames_Success(
+        List<Post> posts,
+        User user,
+        List<Tag> tags,
+        String keyword,
+        List<PostResponseDto> expected
+    ) {
+        userRepository.save(user);
+        tags.forEach(tagRepository::save);
+        posts.forEach(postRepository::save);
+
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        List<String> keywords = List.of(keyword.split(" "));
+        List<Post> savedPosts = postRepository.findAllPostsByTagNames(keywords, pageRequest);
+
+        List<PostResponseDto> actual =
+            PostDtoAssembler.assembleFrom(user, false, savedPosts);
+
+        actual.sort(comparing(PostResponseDto::getAuthorName));
+        expected.sort(comparing(PostResponseDto::getAuthorName));
+
+        assertThat(actual)
+            .usingRecursiveComparison()
+            .ignoringFields("id", "createdAt", "updatedAt")
+            .isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> getPostsAndTagsForSearchPostByTagName() {
+        return Stream.of(
+            createArgument("tag1", 0),
+            createArgument("tag1 tag9", 0, 2),
+            createArgument("tag3 tag1", 0),
+            createArgument("tag1 tag4", 0, 1),
+            createArgument("tag10"),
+            createArgument("tag10 tag1", 0)
+        );
+    }
+
+    private static Arguments createArgument(String keyword, int... expectedIndex) {
+        User user = UserFactory.user("testUser");
+
+        Tag[] tags = IntStream.rangeClosed(1, 9)
+            .mapToObj(i -> new Tag(String.format("tag%d", i)))
+            .toArray(Tag[]::new);
+
+        Post[] posts = new Post[]{
+            Post.builder()
+                .content("testContent1")
+                .githubRepoUrl("https://github.com/bperhaps1")
+                .author(user)
+                .tags(tags[0], tags[1], tags[2])
+                .build(),
+            Post.builder()
+                .content("testContent2")
+                .githubRepoUrl("https://github.com/bperhaps2")
+                .author(user)
+                .tags(tags[3], tags[4], tags[5])
+                .build(),
+            Post.builder()
+                .content("testContent3")
+                .githubRepoUrl("https://github.com/bperhaps3")
+                .author(user)
+                .tags(tags[6], tags[7], tags[8])
+                .build()
+        };
+
+        List<Post> expected = Arrays.stream(expectedIndex)
+            .mapToObj(i -> posts[i])
+            .collect(toList());
+
+        return Arguments.of(
+            List.of(posts[0], posts[1], posts[2]),
+            user,
+            List.of(tags),
+            keyword,
+            PostDtoAssembler.assembleFrom(user, false, expected)
+        );
+    }
+
     private void flushAndClear() {
         testEntityManager.flush();
         testEntityManager.clear();
     }
 }
+
