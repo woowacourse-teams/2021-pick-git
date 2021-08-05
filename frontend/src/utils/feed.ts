1cc6608823e0570aa8268590f5d72ad4992f5bb3
Tanney <dhxodms89@gmail.com>
2021-08-05 12:44:30 +0900

    refactor: getPostsFromPages함수에서 null값도 고려하도록 수정

--- frontend/src/utils/feed.ts
+++ frontend/src/utils/feed.ts
@@ -1,8 +1,8 @@
 import { Post } from "../@types";
 import { removeDuplicatedData } from "./data";
 
-export const getPostsFromPages = (postsPages: Post[][]) => {
-  const posts = postsPages.reduce((acc, postPage) => acc.concat(postPage), []);
+export const getPostsFromPages = (postsPages: (Post[] | null)[]) => {
+  const posts = postsPages.map((page) => page ?? []).reduce((acc, postPage) => acc.concat(postPage), []);
 
   return removeDuplicatedData<Post>(posts, (post) => post.id);
 };
