12b1c1b4dc4938ca55f3b09b07bca0f82d01496e
Tanney <dhxodms89@gmail.com>
2021-08-07 01:26:12 +0900

    fix: 팔로우 목록 패딩 조정

--- frontend/src/pages/FollowingUserListPage/FollowingList.style.ts
+++ frontend/src/pages/FollowingUserListPage/FollowingList.style.ts
@@ -3,6 +3,7 @@ import { Page } from "../../components/@styled/layout";
 
 export const Container = styled(Page)<React.CSSProperties>`
   background-color: ${({ theme }) => theme.color.white};
-  padding: 0 1.4375rem;
+  padding-left: 1.4375rem;
+  padding-right: 1.4375rem;
   height: 100vh;
 `;
