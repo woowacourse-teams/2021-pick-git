4933510ee91d7da67427f36c14ac196182b80dd8
Minsung <sns2831@gmail.com>
2021-08-11 23:41:29 +0900

    feat: ForGuestScanner 구현, ForGuest 어노테이션이 붙은 메소드를 추출한다.

---
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/config/auth_interceptor_regester/scanner/ForGuestScanner.java
@@ -0,0 +1,16 @@
+package com.woowacourse.pickgit.config.auth_interceptor_regester.scanner;
+
+import static java.util.stream.Collectors.toList;
+
+import com.woowacourse.pickgit.config.auth_interceptor_regester.ForGuest;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+public class ForGuestScanner {
+    public List<Method> parseMethods(Class<?> controller) {
+        return Arrays.stream(controller.getMethods())
+            .filter(method -> method.isAnnotationPresent(ForGuest.class))
+            .collect(toList());
+    }
+}
