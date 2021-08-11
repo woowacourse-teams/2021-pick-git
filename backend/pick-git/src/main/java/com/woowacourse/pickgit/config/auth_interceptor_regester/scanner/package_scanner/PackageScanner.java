5a919f4fde44d857ddd33ea458432fdd0982807e
Minsung <sns2831@gmail.com>
2021-08-11 23:36:01 +0900

    feat: PackageScanner 구현, 폴더를 순회하며 클래스 이름을 추출한다.

---
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/config/auth_interceptor_regester/scanner/package_scanner/PackageScanner.java
@@ -0,0 +1,35 @@
+package com.woowacourse.pickgit.config.auth_interceptor_regester.scanner.package_scanner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PackageScanner {
+
+    private final Path rootPath;
+    private final SourceVisitor sourceVisitor;
+
+    public PackageScanner(SourceVisitor sourceVisitor) {
+        this(
+            Paths.get(".").normalize().toAbsolutePath(),
+            sourceVisitor
+        );
+    }
+
+    public PackageScanner(Path rootPath, SourceVisitor sourceVisitor) {
+        this.rootPath = rootPath;
+        this.sourceVisitor = sourceVisitor;
+    }
+
+    public List<String> getAllClassNames() {
+        try {
+            Files.walkFileTree(rootPath, sourceVisitor);
+            return sourceVisitor.getClassPaths();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Interceptor register: 파일 순회 오류", e);
+        }
+    }
+
+}
