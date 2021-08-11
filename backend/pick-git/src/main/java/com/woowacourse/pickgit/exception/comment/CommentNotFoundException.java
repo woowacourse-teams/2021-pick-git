a7a9e97562b8dc53837c5fc0be718dd61ff6f186
Daeun Lee <leede0418@likelion.org>
2021-08-11 23:46:48 +0900

    test: 통합 테스트 작성 (#370)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/comment/CommentNotFoundException.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/comment/CommentNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class CommentNotFoundException extends CommentException {
 
-    private static final String CODE = "P0001";
+    private static final String CODE = "P0008";
     private static final String MESSAGE = "해당 댓글이 존재하지 않습니다.";
 
     public CommentNotFoundException() {
