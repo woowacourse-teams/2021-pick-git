0856bd08f425f8ce649919ad527ed7213d9708a1
Daeun Lee <leede0418@likelion.org>
2021-08-12 17:01:30 +0900

    refactor: 추상 클래스 생성자 public -> protected 변경 (#370)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/comment/CommentException.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/exception/comment/CommentException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 public abstract class CommentException extends ApplicationException {
 
-    public CommentException(String errorCode, HttpStatus httpStatus, String message) {
+    protected CommentException(String errorCode, HttpStatus httpStatus, String message) {
         super(errorCode, httpStatus, message);
     }
 }
