709ef2156a97eca9e93aa2ae1646a55db81da80f
Daeun Lee <leede0418@likelion.org>
2021-08-12 23:52:58 +0900

    fix: CORS 버그 해결 (#447)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/comment/presentation/CommentController.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/comment/presentation/CommentController.java
@@ -14,6 +14,7 @@ import com.woowacourse.pickgit.comment.presentation.dto.response.CommentResponse
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api")
 @RestController
+@CrossOrigin(value = "*")
 public class CommentController {
 
     private final CommentService commentService;
