6f67510284a1c402d921d814c416547e1d7da21f
swon3210 <swon3210@gmail.com>
2021-09-13 03:11:02 +0900

    feat: SVGIcon 컴포넌트 구현
    - SVGIcon 컴포넌트를 통해 각각의 Icon 을 import 하지 않아도 icon 컴포넌트를 사용할 수 있도록 설정

---
+++ frontend/src/components/@shared/SVGIcon/SVGIcon.tsx
@@ -0,0 +1,43 @@
+import styled, { css, CSSProp } from "styled-components";
+import * as icons from "../../../assets/icons";
+
+type IconType = keyof typeof icons;
+
+export const iconTypes = Object.keys(icons) as IconType[];
+
+export type Props = {
+  /** 사용 할 아이콘 타입 */
+  icon: IconType;
+  /** 클릭 이벤트 핸들러 */
+  onClick?: () => void;
+  cssProp?: CSSProp;
+};
+
+const SVGIconWrapper = styled.div<{ isClickable: boolean; cssProp?: CSSProp }>(
+  ({ cssProp, isClickable }) => css`
+    ${cssProp}
+    display: inline-block;
+
+    transition: opacity 0.5s;
+
+    ${isClickable &&
+    ` 
+      cursor: pointer;
+      :hover {
+        opacity: 0.5;
+      }
+    `}
+  `
+);
+
+const SVGIcon = ({ icon, onClick, cssProp }: Props) => {
+  const SVGIcon = icons[icon];
+
+  return (
+    <SVGIconWrapper isClickable={!!onClick} onClick={onClick} cssProp={cssProp}>
+      <SVGIcon style={{ margin: "0px" }} />
+    </SVGIconWrapper>
+  );
+};
+
+export default SVGIcon;
