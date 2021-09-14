b37fcc3a6f91b88aa73a73df70d36b39f33d49dc
Jinhong <xlffm3@gmail.com>
2021-09-14 18:19:02 +0900

    style: 쿼리 v1, v2 분리 (#504)

--- backend/pick-git/src/main/resources/db/migration/V1__init.sql
+++ backend/pick-git/src/main/resources/db/migration/V1__init.sql
@@ -1,26 +1,9 @@
 create table comment
 (
-    id      bigint not null auto_increment,
-    content longtext   not null,
+    id      bigint   not null auto_increment,
+    content longtext not null,
     post_id bigint,
     user_id bigint,
-    primary key (id)
-);
-
-create table contact
-(
-    id           bigint       not null auto_increment,
-    category     varchar(255) not null,
-    value        varchar(255) not null,
-    portfolio_id bigint       not null,
-    primary key (id)
-);
-
-create table description
-(
-    id      bigint not null auto_increment,
-    value   varchar(255),
-    item_id bigint,
     primary key (id)
 );
 
@@ -40,28 +23,11 @@ create table image
     primary key (id)
 );
 
-create table item
-(
-    id         bigint not null auto_increment,
-    category   varchar(255),
-    section_id bigint,
-    primary key (id)
-);
-
 create table likes
 (
     id      bigint not null auto_increment,
     post_id bigint,
     user_id bigint,
-    primary key (id)
-);
-
-create table portfolio
-(
-    id                  bigint       not null auto_increment,
-    introduction        varchar(255),
-    profile_image_shown boolean      not null,
-    profile_image_url   varchar(255) not null,
     primary key (id)
 );
 
@@ -81,35 +47,6 @@ create table post_tag
     id      bigint not null auto_increment,
     post_id bigint not null,
     tag_id  bigint not null,
-    primary key (id)
-);
-
-create table project
-(
-    id           bigint       not null auto_increment,
-    content      varchar(255),
-    end_date     timestamp,
-    image_url    varchar(255) not null,
-    name         varchar(255) not null,
-    start_date   timestamp,
-    type         integer      not null,
-    portfolio_id bigint       not null,
-    primary key (id)
-);
-
-create table project_tag
-(
-    id         bigint not null auto_increment,
-    project_id bigint,
-    tag_id     bigint,
-    primary key (id)
-);
-
-create table section
-(
-    id           bigint       not null auto_increment,
-    name         varchar(255) not null,
-    portfolio_id bigint,
     primary key (id)
 );
 
@@ -150,16 +87,6 @@ alter table comment
         foreign key (user_id)
             references user (id);
 
-alter table contact
-    add constraint fk_contact_to_portfolio
-        foreign key (portfolio_id)
-            references portfolio (id);
-
-alter table description
-    add constraint fk_description_to_item
-        foreign key (item_id)
-            references item (id);
-
 alter table follow
     add constraint fk_follow_to_source
         foreign key (source_id)
@@ -174,11 +101,6 @@ alter table image
     add constraint fk_image_to_post
         foreign key (post_id)
             references post (id);
-
-alter table item
-    add constraint fk_item_to_section
-        foreign key (section_id)
-            references section (id);
 
 alter table likes
     add constraint fk_like_to_post
@@ -204,23 +126,3 @@ alter table post_tag
     add constraint fk_post_tag_to_tag
         foreign key (tag_id)
             references tag (id);
-
-alter table project
-    add constraint fk_project_to_portfolio
-        foreign key (portfolio_id)
-            references portfolio (id);
-
-alter table project_tag
-    add constraint fk_project_tag_to_project
-        foreign key (project_id)
-            references project (id);
-
-alter table project_tag
-    add constraint fk_project_tag_to_tag
-        foreign key (tag_id)
-            references tag (id);
-
-alter table section
-    add constraint fk_section_to_portfolio
-        foreign key (portfolio_id)
-            references portfolio (id);
