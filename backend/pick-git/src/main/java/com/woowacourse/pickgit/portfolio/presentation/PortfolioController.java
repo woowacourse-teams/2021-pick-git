143c10baecb08bdfa9fc6919bbabcf143b96b089
LEE DAEUN <leede0418@gmail.com>
2021-09-16 22:50:57 +0900

    fix: 포트폴리오 기능 버그 해결 (#521) (#522)

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/portfolio/presentation/PortfolioController.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/portfolio/presentation/PortfolioController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/portfolio")
+@RequestMapping("/api/portfolios")
 @CrossOrigin(value = "*")
 public class PortfolioController {
 
