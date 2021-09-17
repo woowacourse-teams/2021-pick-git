763ab851ca4948ddcb73d9ebe8948965b25a8835
swon3210 <swon3210@gmail.com>
2021-09-17 10:18:26 +0900

    fix: 프로젝트 섹션 추가 시 인피니트 스크롤링이 적용되지 않는 문제 해결
    - 포트폴리오 기능 API 연동 임시 해제

--- frontend/src/components/PostSelector/PostSelector.tsx
+++ frontend/src/components/PostSelector/PostSelector.tsx
@@ -2,13 +2,16 @@ import { InfiniteData } from "react-query";
 import { PortfolioProject, Post } from "../../@types";
 import { getItemsFromPages } from "../../utils/infiniteData";
 import { Container, Grid, GridItem } from "./PostSelector.style";
+import InfiniteScrollContainer from "../@shared/InfiniteScrollContainer/InfiniteScrollContainer";
 
 export interface Props {
   infinitePostsData: InfiniteData<Post[]> | undefined;
+  isFetchingNextPage: boolean;
+  onIntersect: () => void;
   onPostSelect: (post: Post) => void;
 }
 
-const PostSelector = ({ onPostSelect, infinitePostsData }: Props) => {
+const PostSelector = ({ infinitePostsData, isFetchingNextPage, onPostSelect, onIntersect }: Props) => {
   if (!infinitePostsData) {
     return <div>게시물 정보를 가져올 수 없습니다.</div>;
   }
@@ -17,11 +20,13 @@ const PostSelector = ({ onPostSelect, infinitePostsData }: Props) => {
 
   return (
     <Container>
-      <Grid>
-        {posts.map((post) => (
-          <GridItem imageUrl={post.imageUrls[0]} onClick={() => onPostSelect(post)}></GridItem>
-        ))}
-      </Grid>
+      <InfiniteScrollContainer isLoaderShown={isFetchingNextPage} onIntersect={onIntersect}>
+        <Grid>
+          {posts.map((post) => (
+            <GridItem imageUrl={post.imageUrls[0]} onClick={() => onPostSelect(post)}></GridItem>
+          ))}
+        </Grid>
+      </InfiniteScrollContainer>
     </Container>
   );
 };
