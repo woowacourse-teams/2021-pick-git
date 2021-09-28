76202892f853c44c6ff00b5d3736e80c450bfcde
Daeun Lee <leede0418@likelion.org>
2021-09-28 16:16:11 +0900

    refactor: 변수명 변경

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/portfolio/domain/common/UpdateUtil.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/portfolio/domain/common/UpdateUtil.java
@@ -12,55 +12,65 @@ public class UpdateUtil {
     private UpdateUtil() {
     }
 
-    public static <T extends Updatable<T>> void execute(List<T> origin, List<T> source) {
-        List<UpdatableProxy<T>> originProxies = toProxy(origin);
-        List<UpdatableProxy<T>> sourceProxies = toProxy(source);
+    public static <T extends Updatable<T>> void execute(List<T> origins, List<T> sources) {
+        List<UpdatableProxy<T>> originProxies = toProxy(origins);
+        List<UpdatableProxy<T>> sourceProxies = toProxy(sources);
 
         update(originProxies, sourceProxies);
         delete(originProxies, sourceProxies);
         create(originProxies, sourceProxies);
 
-        resetOrigin(origin, originProxies);
+        reset(origins, originProxies);
     }
 
-    private static <T extends Updatable<T>> List<UpdatableProxy<T>> toProxy(
-        List<T> origin) {
-        return origin.stream()
+    private static <T extends Updatable<T>> List<UpdatableProxy<T>> toProxy(List<T> values) {
+        return values.stream()
             .map(UpdatableProxy::new)
             .collect(toList());
     }
 
-    private static <T extends Updatable<T>> void update(List<UpdatableProxy<T>> origin,
-        List<UpdatableProxy<T>> source) {
-        Map<UpdatableProxy<T>, UpdatableProxy<T>> origins = origin.stream()
+    private static <T extends Updatable<T>> void update(
+        List<UpdatableProxy<T>> originProxies,
+        List<UpdatableProxy<T>> sourceProxies
+    ) {
+        Map<UpdatableProxy<T>, UpdatableProxy<T>> originsWithProxy = originProxies.stream()
             .collect(toMap(Function.identity(), Function.identity()));
 
-        source.stream()
-            .filter(origins::containsKey)
-            .forEach(s -> origins.get(s).update(s));
+        sourceProxies.stream()
+            .filter(originsWithProxy::containsKey)
+            .forEach(sourceProxy -> originsWithProxy.get(sourceProxy).update(sourceProxy));
     }
 
-    private static <T extends Updatable<T>> void delete(List<UpdatableProxy<T>> origin,
-        List<UpdatableProxy<T>> source) {
-        origin.removeIf(o -> !source.contains(o));
+    private static <T extends Updatable<T>> void delete(
+        List<UpdatableProxy<T>> originProxies,
+        List<UpdatableProxy<T>> sourceProxies
+    ) {
+        originProxies.removeIf(origin -> !sourceProxies.contains(origin));
     }
 
-    private static <T extends Updatable<T>> void create(List<UpdatableProxy<T>> origin,
-        List<UpdatableProxy<T>> source) {
-        source.stream()
-            .filter(s -> !origin.contains(s))
-            .forEach(origin::add);
+    private static <T extends Updatable<T>> void create(
+        List<UpdatableProxy<T>> originProxies,
+        List<UpdatableProxy<T>> sourceProxies
+    ) {
+        sourceProxies.stream()
+            .filter(source -> !originProxies.contains(source))
+            .forEach(originProxies::add);
     }
 
-    private static <T extends Updatable<T>> void resetOrigin(List<T> origin,
-        List<UpdatableProxy<T>> originProxies) {
-        origin.clear();
-        List<T> updatedOrigin = extractTarget(originProxies);
-        origin.addAll(updatedOrigin);
+    private static <T extends Updatable<T>> void reset(
+        List<T> origins,
+        List<UpdatableProxy<T>> originProxies
+    ) {
+        origins.clear();
+
+        List<T> targets = getTargets(originProxies);
+
+        origins.addAll(targets);
     }
 
-    private static <T extends Updatable<T>> List<T> extractTarget(
-        List<UpdatableProxy<T>> originProxies) {
+    private static <T extends Updatable<T>> List<T> getTargets(
+        List<UpdatableProxy<T>> originProxies
+    ) {
         return originProxies.stream()
             .map(UpdatableProxy::getTarget)
             .collect(toList());
