2d33cedb5d7c3d353adfe59af75835ade6a20485
Minsung Son <sns2831@gmail.com>
2021-10-04 03:15:18 +0900

    refactor: JpaRepository 합치기

--- backend/pick-git/src/test/java/com/woowacourse/pickgit/unit/user/domain/UserRepositoryTest.java
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/unit/user/domain/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package com.woowacourse.pickgit.unit.user.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.woowacourse.pickgit.common.factory.UserFactory;
+import com.woowacourse.pickgit.config.JpaTestConfiguration;
 import com.woowacourse.pickgit.exception.user.InvalidUserException;
 import com.woowacourse.pickgit.post.domain.Post;
 import com.woowacourse.pickgit.post.domain.repository.PostRepository;
@@ -16,9 +17,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+@Import(JpaTestConfiguration.class)
 @DataJpaTest
 class UserRepositoryTest {
 
