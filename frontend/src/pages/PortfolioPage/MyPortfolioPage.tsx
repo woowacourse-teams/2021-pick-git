2673ed9233467be56ba359085500603b562f8da1
swon3210 <swon3210@gmail.com>
2021-10-14 16:21:14 +0900

    fix: console 에 뜨는 동적 컴포넌트 key 관련 에러 제거

--- frontend/src/pages/PortfolioPage/MyPortfolioPage.tsx
+++ frontend/src/pages/PortfolioPage/MyPortfolioPage.tsx
@@ -201,6 +201,11 @@ const MyPortfolioPage = () => {
     }
   };
 
+  const handleWheelEvent: React.WheelEventHandler<HTMLElement> = (event) => {
+    event.preventDefault();
+    console.log("wheel");
+  };
+
   useEffect(() => {
     if (!remotePortfolio || !remotePortfolio.updatedAt) {
       return;
@@ -271,7 +276,7 @@ const MyPortfolioPage = () => {
           onUploadPortfolio={handleUploadPortfolio}
         />
       </ScrollActiveHeader>
-      <Container ref={containerRef}>
+      <Container ref={containerRef} onWheel={handleWheelEvent}>
         <FullPage isVerticalCenter={true}>
           <ToggleButton
             toggleButtonText="프로필 사진 보이기"
