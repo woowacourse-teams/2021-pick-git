141646ffbb7fa9d8dcd6060390b990c2c7d81125
Minsung Son <sns2831@gmail.com>
2021-10-22 18:21:46 +0900

    refactor: 변수명 변경

--- backend/pick-git/src/main/java/com/woowacourse/pickgit/user/infrastructure/contribution/PlatformContributionExtractor.java
+++ backend/pick-git/src/main/java/com/woowacourse/pickgit/user/infrastructure/contribution/PlatformContributionExtractor.java
@@ -19,6 +19,6 @@ public interface PlatformContributionExtractor {
         String accessToken,
         String username,
         Map<ContributionCategory, Integer> bucket,
-        CountDownLatch countDownLatch
+        CountDownLatch latch
     );
 }
