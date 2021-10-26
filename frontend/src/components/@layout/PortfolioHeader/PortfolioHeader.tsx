90f85ed8154130bfa92262aa1ec60d13c62a4a59
swon3210 <swon3210@gmail.com>
2021-10-26 15:50:30 +0900

    refactor: 필요없는 주석 삭제

--- frontend/src/components/@layout/PortfolioHeader/PortfolioHeader.tsx
+++ frontend/src/components/@layout/PortfolioHeader/PortfolioHeader.tsx
@@ -30,13 +30,6 @@ const PortfolioHeader = ({ isButtonsShown = true }: Props) => {
     history.goBack();
   };
 
-  // const dropdownListItems: DropDownItem[] = [
-  //   { text: "연착처 업데이트", onClick: onSetPortfolioContacts },
-  //   { text: "프로젝트 추가", onClick: onAddPortfolioProject },
-  //   { text: "섹션 추가", onClick: onAddPortfolioSection },
-  //   { text: "전체 업로드", onClick: onUploadPortfolio },
-  // ];
-
   return (
     <Container>
       <HeaderContentWrapper>
@@ -45,9 +38,6 @@ const PortfolioHeader = ({ isButtonsShown = true }: Props) => {
         </GoBackLinkButton>
         {isButtonsShown && (
           <HeaderButtonsWrapper>
-            {/* <DropDown items={dropdownListItems} cssProp={DropDownCSS}>
-              작성란 추가
-            </DropDown> */}
             <Button kind="roundedInline" backgroundColor={color.primaryColor} color={color.white}>
               PDF로 인쇄
               {/* <PDFDownloadLink
