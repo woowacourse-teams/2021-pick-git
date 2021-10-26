a913b169a273f9a76bc30ee3b9c3ac1a05e1259f
Minsung Son <sns2831@gmail.com>
2021-10-26 22:21:29 +0900

    refactor: flyway 추가

---
+++ backend/pick-git/src/main/resources/db/migration/V7__update_portfolio_column_type.sql
@@ -0,0 +1,6 @@
+alter table comment modify content longtext;
+alter table description modify value longtext;
+alter table item modify category longtext;
+alter table portfolio modify introduction longtext;
+alter table post modify content longtext;
+alter table project modify content longtext;
