4b9eac76ca9c8b0b919ede5c15a5899d65f89665
Jinhong <xlffm3@gmail.com>
2021-10-26 15:47:07 +0900

    refactor: es delete query 삭제 (#568)

--- backend/pick-git/src/test/java/com/woowacourse/pickgit/config/SearchEngineCleaner.java
+++ backend/pick-git/src/test/java/com/woowacourse/pickgit/config/SearchEngineCleaner.java
@@ -1,47 +1,20 @@
 package com.woowacourse.pickgit.config;
 
+import com.woowacourse.pickgit.user.domain.search.UserSearchEngine;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 @Profile("test")
 @Component
 public class SearchEngineCleaner implements InitializingBean {
 
-    @Value("${security.elasticsearch.host}")
-    private String elasticSearchHost;
+    @Autowired
+    private UserSearchEngine userSearchEngine;
 
     @Override
-    public void afterPropertiesSet() throws Exception {
-        clearUsers();
-    }
-
-    public void clearUsers() {
-        String query = "{\n"
-            + "  \"query\": {\n"
-            + "    \"range\" : {\n"
-            + "        \"id\" : {\n"
-            + "           \"gte\" : 1\n"
-            + "        }\n"
-            + "    }\n"
-            + "  }\n"
-            + "}";
-        String url = "http://" + elasticSearchHost + "/users/_delete_by_query";
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity<String> httpEntity = new HttpEntity<>(query, headers);
-
-        try {
-            new RestTemplate().exchange(url, HttpMethod.POST, httpEntity, String.class);
-        } catch (HttpClientErrorException ignored) {
-            ignored.printStackTrace();
-        }
+    public void afterPropertiesSet() {
+        userSearchEngine.deleteAll();
     }
 }
